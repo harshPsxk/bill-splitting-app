d320d4a02f5996abf2744bf5c4889bc7c3a9ecd6
Harsh Pratap <harshPsingh@live.com>
2024-03-27 13:29:47 +0530

    finished with eslint and prettier config

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Welcome to Bill Splitting app
+      </div>
+    </div>
+  );
+}
+
+export default App;
